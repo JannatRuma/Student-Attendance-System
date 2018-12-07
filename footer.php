5a9a779769651fc3dc80349693bd671e5b43489f
Jannat Binta Alam <jannat1ruma@gmail.com>
2018-12-07 12:04:00 +0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,11 @@
+ <div class="well">
+            <h3>
+                Website: www.janalam.xyz 
+                <span class="pull-right"> Like us: www.facebook.com/jannat.ruma.7</span>
+            </h3>
+        </div>
+        
+    </div>
+	
+</body>
+</html>
