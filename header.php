aa1603e4d88a5542b96e055a66523d3ceb3105da
Jannat Binta Alam <jannat1ruma@gmail.com>
2018-12-07 12:04:15 +0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title> Student Attendance System </title>
+    <link rel="stylesheet" href="bootstrap.min.css" media="screen" title="no title"/>
+    
+    <script type="text/javascript" src="jquery.min.js"></script>
+    <script type="text/javascript" src="bootstrap.min.css"></script>
+</head>
+    
+<body>
+    <div class="container">
+        <div class="well text-center">
+            <h2>Student Attendance System</h2>
+        </div>
